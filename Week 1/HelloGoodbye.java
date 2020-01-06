7687c03077fc7d2c0a0f212da7b5fe2e232bad74
weesheng.chu <weesheng10@gmail.com>
2020-01-06 19:45:53 +0800

    Week 1

---
+++ Week 1/HelloGoodbye.java
@@ -0,0 +1,7 @@
+public class HelloGoodbye {
+    public static void main(String[] args) {
+
+        System.out.println("Hello " + args[0] + " and " + args[1] + ".");
+        System.out.println("Hello " + args[1] + " and " + args[0] + ".");
+    }
+}
